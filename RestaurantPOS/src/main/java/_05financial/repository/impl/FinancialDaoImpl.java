8b8677bb5a68e3e95dd2248e50b00b2d266d9f64
廖佩瑜 <s88021401@gmail.com>
2019-03-08 13:32:21 +0800

    FinancialController add method dailyReportGetDat, fix web Url  --0308

--- RestaurantPOS/src/main/java/_05financial/repository/impl/FinancialDaoImpl.java
+++ RestaurantPOS/src/main/java/_05financial/repository/impl/FinancialDaoImpl.java
@@ -3,6 +3,7 @@ package _05financial.repository.impl;
 import java.sql.Timestamp;
 import java.util.List;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,11 +17,25 @@ public class FinancialDaoImpl implements FinancialDao{
 	public SessionFactory getFactory() {
 		return factory;
 	}
-
+	
+	//getOrder
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<OrderBean> getAllOrder() {
-		
-		return null;
+		Session session = factory.getCurrentSession();
+		String hql="FROM Order";
+		List<OrderBean> list = session.createQuery(hql).getResultList();
+		System.out.println("getAllOrder is here!");
+		return list;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<OrderBean> getOrderByDate(Timestamp Date1, Timestamp Date2) {
+		String hql= "FROM OrderBean o WHERE o.Date1 = :Date1";
+		Session session = factory.getCurrentSession();
+		List<OrderBean> list = session.createQuery(hql).setParameter("Date1", Date1).getResultList();
+		return list;
 	}
 	
 
