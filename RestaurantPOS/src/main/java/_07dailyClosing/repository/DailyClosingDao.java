4e11e36bcbe3c3abd6a9cd66e59bb68a77f444e5
朱凱頤 <Kai@eeit105>
2019-03-15 16:40:17 +0800

    20190315 1640

--- RestaurantPOS/src/main/java/_07dailyClosing/repository/DailyClosingDao.java
+++ RestaurantPOS/src/main/java/_07dailyClosing/repository/DailyClosingDao.java
@@ -1,9 +1,29 @@
 package _07dailyClosing.repository;
 
+import java.sql.Date;
+
+import _00model.CumulativeTurnoverBean;
+
 public interface DailyClosingDao {
 
 	void stringToDate(String Date);
 
 	Integer getTodayTurnover(String date);
+
+	Integer getMaxIdNumber();
+
+	Integer getCTPrevious();
+
+	void addDailyClosing(CumulativeTurnoverBean cumulativeTurnoverBean);
+
+
+
+	Integer completeCheckToday(Date today);
+
+	CumulativeTurnoverBean getAllById(Integer id);
+
+
+
+
 
 }
