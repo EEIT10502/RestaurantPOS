8d565765321ca8e1936895dfa17ec94bd8674267
蔣耀德 <eeit10504@gmail.com>
2019-03-21 15:46:39 +0800

    0321 打卡機小修改(empNo  String->Integer)

--- RestaurantPOS/src/main/java/_04schedule/repository/impl/ScheduleDaoImpl.java
+++ RestaurantPOS/src/main/java/_04schedule/repository/impl/ScheduleDaoImpl.java
@@ -35,7 +35,7 @@ public class ScheduleDaoImpl implements ScheduleDao{
 		try {
 			String hql="FROM EmployeeBean WHERE empNo = :empNo";
 			employeeBean = (EmployeeBean) session.createQuery(hql).
-												  setParameter("empNo", empNo).
+												  setParameter("empNo", Integer.parseInt(empNo)).
 												  uniqueResult();
 			
 		} catch (Exception e) {
