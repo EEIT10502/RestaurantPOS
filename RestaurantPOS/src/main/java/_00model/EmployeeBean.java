1db16fe07e31a06f2d0cdfe5d63f0d517bc1dcff
耀德 <tso1963172@gmail.com>
2019-03-22 02:12:15 +0800

    0322 02:12系統測試中

--- RestaurantPOS/src/main/java/_00model/EmployeeBean.java
+++ RestaurantPOS/src/main/java/_00model/EmployeeBean.java
@@ -175,38 +175,6 @@ public class EmployeeBean implements Serializable {
 		this.img = img;
 	}
 
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("EmployeeBean [empId=");
-		builder.append(empId);
-		builder.append(", empNo=");
-		builder.append(empNo);
-		builder.append(", empName=");
-		builder.append(empName);
-		builder.append(", gender=");
-		builder.append(gender);
-		builder.append(", position=");
-		builder.append(position);
-		builder.append(", tel=");
-		builder.append(tel);
-		builder.append(", addr=");
-		builder.append(addr);
-		builder.append(", remark=");
-		builder.append(remark);
-		builder.append(", status=");
-		builder.append(status);
-		builder.append(", img=");
-		builder.append(img);
-		builder.append(", fileName=");
-		builder.append(fileName);
-		builder.append(", empImg=");
-		builder.append(empImg);
-		builder.append("]");
-		return builder.toString();
-	}
-	
 	//對應到Calendar 
 	@OneToMany(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
 	@JoinColumn(name="empId")
